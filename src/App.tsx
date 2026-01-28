c63758fe40ad3734b40a6748c979e89a381354f2
23ohjaewon <sk91015956@gmail.com>
2026-01-29 00:35:19 +0900

    추천 검색, 작성, 완료 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import CommunityPage from "./pages/Community/CommunityPage";
-import BookCommunityPage from "./pages/Community/BookCommunityPage.tsx";
-import RecommendWritePage from "./pages/Recommend/RecommendwritePage.tsx";
-import RecommendCompletePage from "./pages/Recommend/RecommendCompletePage.tsx";
-
+import './App.css';
 
 function App() {
-
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/community" element={<CommunityPage />} />
-
-        <Route path="/community/book/:bookId" element={<BookCommunityPage />} />
-
-        <Route path="/recommend/write" element={<RecommendWritePage />} />
-
-        <Route path="/recommend/complete" element={<RecommendCompletePage />} />
-
-
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>Hello React</h1>
+    </>
   );
 }
 
 export default App;
-
