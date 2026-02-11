ad5a4389537128e578e855736ad963974561ebe8
양희진 <didgmlwls54@gmail.com>
2026-02-11 10:39:44 +0900

    라우트 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 // router/index.tsx (예시)
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from "react-router-dom";
 
 import StartPage from "../pages/Login/StartPage";
 import ProtectedRoute from "../components/ProtectedRoute";
@@ -14,8 +14,6 @@ import PasswordEditPage from "../pages/MyPage/PasswordEditPage";
 import ReadingQuestionPage from "../pages/MyPage/ReadingQuestionPage";
 import MyReadingMemoPage from "../pages/MyPage/MyReadingMemoPage";
 import EditMenuPage from "../pages/MyPage/EditMenuPage";
-import MyPageFindPage from "../pages/Login/FindPage";
-
 import KakaoRedirect from "../layouts/kakaoRedirect";
 import FindIdPage from "../pages/Login/FIndIdPage";
 import FindPasswordPage from "../pages/Login/FindPasswordPage";
@@ -28,27 +26,28 @@ import ReviewCommentPage from "../pages/MyPage/ReviewCommentPage";
 import ReadingRecordPage from "../pages/MyPage/ReadingRecordPage";
 import ReviewDetailPage from "../pages/MyPage/ReviewDetailPage";
 import GlobalSpinner from "../components/common/GlobalSpinner";
+import FindPage from "../pages/Login/FindPage";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <GlobalSpinner />
-            <Route path="/" element={<StartPage />} />
+
+            <Route path="/start" element={<StartPage />} />
             <Route path="/oauth/kakao" element={<KakaoRedirect />} />
             <Route path="/auth/login/local" element={<LoginPage />} />
-
             <Route path="/signup/step1" element={<SignupPage />} />
             <Route path="/signup/step2" element={<SignupPage2 />} />
             <Route path="/signup/step3" element={<SignupPage3 />} />
             <Route path="/signup/complete" element={<SignupCompletePage />} />
-
-            <Route path="/find/menu" element={<MyPageFindPage />} />
+            <Route path="/find/menu" element={<FindPage />} />
             <Route path="/find-id/email/send" element={<FindIdPage />} />
             <Route path="/find-password/email/send" element={<FindPasswordPage />} />
             <Route path="/find-password/reset" element={<ResetPasswordPage />} />
             <Route path="/policy" element={<PolicyPage />} />
 
             {/* protected (로그인 필요) */}
+
             <Route element={<ProtectedRoute />}>
                 <Route path="/profile/edit/menu" element={<EditMenuPage />} />
                 <Route path="/profile/edit/id" element={<ProfileEditIdPage />} />
@@ -60,11 +59,10 @@ export const router = createBrowserRouter(
                 <Route path="/my-page/menu" element={<MyPageMenuPage />} />
                 <Route path="/questions/me" element={<ReadingQuestionPage />} />
                 <Route path="/reviews/me" element={<ReviewCommentPage />} />
-                <Route path="/reviews/me/detail" element={<ReviewDetailPage />} />
+                <Route path="/reviews/me/:reviewId" element={<ReviewDetailPage />} />
                 <Route path="/memos/me" element={<MyReadingMemoPage />} />
                 <Route path="/members/me/records" element={<ReadingRecordPage />} />
-
-            </Route>
+            </ Route>
         </>
     )
 );
